921806509df58a29010b3f4bc71895e08f690388
ikegami <hiroshi.ikegami@magicdrive.jp>
2012-12-16 23:33:09 +0900

    color

--- status-left.sh
+++ status-left.sh
@@ -28,18 +28,18 @@ register_segment "hostname"
 
 declare -A tmux_session_info
 tmux_session_info+=(["script"]="${segments_path}/tmux_session_info.sh")
-tmux_session_info+=(["foreground"]="colour234,nobold")
-tmux_session_info+=(["background"]="colour255")
+tmux_session_info+=(["foreground"]="colour252,nobold")
+tmux_session_info+=(["background"]="colour60")
 tmux_session_info+=(["separator"]="${separator_right_bold}")
 #tmux_session_info+=(["separator_fg"]="default")
 register_segment "tmux_session_info"
 
-declare -A prefix
-prefix+=(["script"]="${segments_path}/tmux_prefix.sh")
-prefix+=(["foreground"]="colour255")
-prefix+=(["background"]="colour60")
-prefix+=(["separator"]="${separator_right_bold}")
-register_segment "prefix"
+##########declare -A prefix
+##########prefix+=(["script"]="${segments_path}/tmux_prefix.sh")
+##########prefix+=(["foreground"]="colour255")
+##########prefix+=(["background"]="colour60")
+##########prefix+=(["separator"]="${separator_right_bold}")
+##########register_segment "prefix"
 
 #########declare -A ifstat
 #########ifstat+=(["script"]="${segments_path}/ifstat.sh")
