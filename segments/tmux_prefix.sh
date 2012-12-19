f895f84df83a9e3183b12388eff961021e5b6aa9
hiroshi_ikegami <yawgmothgodofphyrexia@gmail.com>
2012-12-20 01:35:24 +0900

    perl to sed

--- segments/tmux_prefix.sh
+++ segments/tmux_prefix.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 # Prints tmux prefix info.
 # Assuems that [ -n "$TMUX"].
-tmux list-keys| grep prefix | perl -pi -e 's/bind-key[\s]*(.*)[\s]send-prefix/$1/'
+#tmux list-keys| grep prefix | perl -pi -e 's/bind-key[\s]*(.*)[\s]send-prefix/$1/'
+tmux list-keys| grep prefix | sed -e 's/bind-key[ \n\r\f\t]*\(.*\)[ \n\r\f\t]send-prefix/\1/'
 exit 0;
